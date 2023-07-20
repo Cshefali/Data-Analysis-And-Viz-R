3a0ccd56a86209295c618d34f6a66da7abdc399c
Shefali Chandra <shefali.chandra98@gmail.com>
2023-07-20 23:26:17 +0530

    added swim plots

---
+++ swimmer_plots.R
@@ -0,0 +1,8 @@
+#Swimmer plots in R
+#Author- Shefali C.
+#Date- July 20, 2023
+
+#Source- https://cran.r-project.org/web/packages/swimplot/vignettes/Introduction.to.swimplot.html#introduction-to-swimmers-plots
+
+library(tidyverse)
+library(swimplot)
