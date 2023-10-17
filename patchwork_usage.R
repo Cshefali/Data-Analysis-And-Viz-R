5d11d07457a63359e7db516074042a3fa053a94f
Shefali Chandra <shefali.chandra98@gmail.com>
2023-10-17 13:32:46 +0530

    added more patchwork applications

--- patchwork_usage.R
+++ patchwork_usage.R
@@ -2,10 +2,12 @@
 #References- 
 #1. https://patchwork.data-imaginist.com/
 #2. https://patchwork.data-imaginist.com/articles/guides/assembly.html
-#Last Update- Oct 16, 2023
+#3. https://patchwork.data-imaginist.com/articles/guides/layout.html
+#Last Update- Oct 17, 2023
 
 library(tidyverse)
 library(patchwork)
+library(ggthemes)
 
 ##NOTE-
 #ggplotify package functions can be used to convert different graphics to grobs
@@ -71,6 +73,15 @@ p1 | (p2/p3)
 (p1 + p2 + p3) + 
   plot_annotation(tag_levels = "1")
 
+##DIFFERENCE BETWEEN '+' AND '|'
+
+#'+'- patchwork assembles the plots in best way possible, trying to fit all in a 
+#square figure.
+p1 + p2 + p3 + p4
+
+#'|' is specific about placing all plots side by side only.
+p1 | p2 | p3 | p4
+
 ##Save a plot assembly and add another one later on
 
 patch1 <- p1 + p2
@@ -80,10 +91,26 @@ p3 + patch1
 #Adding objects other than ggplot objects like grobs
 #here, textgrob
 
-#add text right or left
-patch1 + grid::textGrob(label = '+ adds text horizontally')
-#add text at top or bottom
+##The first element to patchwork operators '+', '/' etc. must be 
+##a ggplot object. If that's not the case, NULL is returned.
+##In order to keep other objects as first one like grobs, base R plot etc.
+##enclose that object inside wrap_elements().
+
+#add text to the right
+p1 + grid::textGrob(label = '+ adds text horizontally')
+#adding text to the left-- this will return NULL
+grid::textGrob(label = "To add text on LHS,\nenclose the textgrob inside wrap_elements()") + p1
+
+#add text to LHS, this will work
+patchwork::wrap_elements(grid::textGrob(label = "Add text to LHS using\nwrap_elements()")) + p1
+
+#add text at bottom
 patch1 / grid::textGrob(label = " '/' adds texts at bottom or top")
+
+#adding text at top
+wrap_elements(grid::textGrob(label = "Text 1")) / p1
+
+
 
 #add a more complex grob like a table to the plot
 p1+ gridExtra::tableGrob(mtcars[1:5, c('mpg', 'disp')])
@@ -92,7 +119,72 @@ p1+ gridExtra::tableGrob(mtcars[1:5, c('mpg', 'disp')])
 #p1 + gridExtra::tableGrob(tibble::rownames_to_column(mtcars[1:5, c('mpg', 'disp')], 'carname'))
 
 #add a base R plot alongside ggplot
+#experiment with par() settings to get perfect alighment
 (p1 + ~plot(mtcars$mpg, mtcars$disp, main = "Plot 2"))+
   plot_annotation(title = "ggplot object with base R plot",
     subtitle = "Note: That tilde '~' is crucial to combining base R plot with ggplot object")
+
+#try altering margins, background color etc. to align base plot with ggplot graph
+old_par <- par(mar = c(0, 2, 0, 0), bg = NA)
+p1 + wrap_elements(panel = ~plot(mtcars$mpg, mtcars$disp, main = "Plot 2"), clip = FALSE)
+par(old_par)
+
+#In case, plots are being automatically generated through a loop or something
+#Combining plots manually is hectic, use wrap_plots() for this
+
+wrap_plots(p1 + p2 + p3 + p4)
+
+##'Nesting the left-hand side'.
+##When a new plot is added from LHS, this plot gets half of the entire plot area
+##And others get small shares of the other half.
+##But if plot is added on RHS, all plots get equal area.
+
+patch2 <- p1 + p2
+
+p3 + patch2 #plot 1 and 2 get nested, meaning they get shared in 50% of space while p3 gets entire 50%
+patch2 + p3 #here, all three get equal area share.
+patch2 - p3 #hyphen is used in nesting cases in LHS.
+#to understand better, LHS nested plots
+
+#the jitter plot gets added to last active plot, which is p2 here.
+p3 + p2 + geom_jitter(data = mtcars, aes(x = mtcars$gear, y= mtcars$disp))
+
+#Accessing individual plots in a patchwork object
+(patch3 <- p1 + p2 + p4)
+
+patch3[[2]] <- patch3[[2]] + theme_economist()
+
+#see patch3 now
+patch3
+
+#Applying themes to ALL or ONLY CURRENT NESTING LEVEL PLOTS
+(patch3 <- p1 + p2 + p4)
+#Apply a theme to all plots using '&'
+patch3 & ggthemes::theme_fivethirtyeight()
+
+#Nested plot-- here plot 1,2 and 4 are nested ones.
+p3 + patch3
+
+#Economist theme gets applied only to the last nested plot, i.e. p3
+(p3 / patch3) * theme_economist()
+
+############################
+##CONTROLLING LAYOUTS
+#https://patchwork.data-imaginist.com/articles/guides/layout.html
+
+#Adding space between plots
+p1 + plot_spacer() + p2 + plot_spacer() + p3 + plot_spacer()
+
+p1 + p2 + p3 + p4 +
+  plot_layout(widths = c(2,1))
+
+p1 + p2 + p3 + p4 +
+  plot_layout(widths = c(1,2))
+
+p1 + p2 + p3 + p4 + 
+  plot_layout(#width of 1st column is twice the width of plots in 2nd col
+              widths = c(2,1),
+              #height of plots in 1st row is thrice the height of plots in 2nd row
+              heights = unit(c(3,1), c("cm", "cm"))
+              )
 
