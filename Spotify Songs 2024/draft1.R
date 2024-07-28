8216d5f96d3bf1e645c31980989e19554e98d18d
Shefali Chandra <shefali.chandra98@gmail.com>
2024-07-28 22:29:57 +0530

    ntbk updated- spotify

---
+++ Spotify Songs 2024/draft1.R
@@ -0,0 +1,44 @@
+library(tidyverse)
+#Last update- July 28, 2024
+
+#data dir
+data_dir <- paste0(getwd(), "/data/")
+
+#guess encoding of the file
+readr::guess_encoding(paste0(data_dir, 'Most Streamed Spotify Songs 2024.csv'))
+
+#read dataset
+data = readr::read_csv(paste0(data_dir,'Most Streamed Spotify Songs 2024.csv'),
+                       locale = locale(encoding = "ISO-8859-1"))
+
+## see all column-names
+colnames(data)
+
+#description
+glimpse(data)
+##OBSERVATIONS:
+# 1. release data is in char format; should be date.
+
+#clean names of columns
+data <- janitor::clean_names(data)
+
+##1. Check date format
+
+#check for all symbols except digits- only "--" & NA present
+unique(gsub(pattern = "\\d+", replacement = "", data$release_date))
+
+##Identify date format- mm-dd-yyyy or dd-mm-yyyy--range is 1-12.
+##first 2 digits represent days
+unique(str_extract(string = data$release_date, pattern = "^\\d+(?=-)"))
+
+##middle 2 digits.--range is from 1 to 31, indicating all months are valid.
+range(unique(as.integer(str_extract(string = data$release_date, 
+                   pattern = "(?<=/)\\d+(?=-)"))))
+
+##last two digits-- checking whether all year entries are valid or not
+#all years are valid.
+unique(as.integer(str_extract(string = data$release_date,
+                   pattern = "(?<=/)\\d{2,4}$")))
+
+#convert release-date column to detected format- dd/mm/yyyy
+data$release_date <- lubridate::dmy(data$release_date)
